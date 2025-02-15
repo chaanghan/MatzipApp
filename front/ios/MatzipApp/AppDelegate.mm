8ed377f5d4ba51a4942763343c63042a756bed91
changhan <momo017097@gmail.com>
2025-02-15 13:26:57 +0900

    setup: google maps 권한 설정

--- front/ios/MatzipApp/AppDelegate.mm
+++ front/ios/MatzipApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyD1wX8w7fseioR7-Tb9ttSh95OaB3_O_rc"]; 
   self.moduleName = @"MatzipApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
